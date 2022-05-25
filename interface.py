af633863b691a13f224091fe45524e8e6ed9e3f6
Priyanka S <89259438+priya158@users.noreply.github.com>
2022-05-25 12:44:23 +0530

    Add files via upload

---
+++ interface.py
@@ -0,0 +1,75 @@
+from Bank import BankAccount
+import datetime
+
+customer_dict = {}              # use account no. as key and class object(customer account) as value
+mobile_acc_link = {}            # use mobile no. as key and store account no. as value, for linking purpose
+
+def new_cust():
+    name = input('Enter the name of customer: ')
+    mobile_no = int(input('Enter the mobile number of customer: '))
+    initial_depo = int(input('Enter the initial deposit amount: '))
+    if initial_depo <= 0:
+        print('Invalid Amount')
+        return
+    pin = int(input('Create PIN: '))
+    customer = BankAccount(name=name, mobile_no=mobile_no, initial_depo=initial_depo, pin=pin)
+    customer_dict[customer.cust_acc_num] = customer                 # acct. no. stored as key and object as value
+    mobile_acc_link[customer.mobile_no] = customer.cust_acc_num     # mobile no. linked
+    x = datetime.datetime.now()
+    print('New User Account Created on',x)
+    print(f'Welcome {customer.name} to State Bank of India. {customer.cust_acc_num} is your account number')
+
+def login():
+    account_no = int(input('Enter your Account Number: '))
+    account_pin = int(input('Enter your Account PIN: '))
+    if account_no in customer_dict.keys() and account_pin == customer_dict[account_no].pin :
+        a = datetime.datetime.now()
+        print(f'\n{customer_dict[account_no].name} Logged in on',a)
+        customer_dict[account_no].basic_details()
+    else:
+        print('Account either not exist or the pin is wrong')
+        return
+    while True:
+        user_input1 = input('''Press 1 for deposit:
+Press 2 for withdrawl:
+Press 3 for money transfer:
+Press 4 to log out\n''')
+        if user_input1 == '1':
+            customer_dict[account_no].deposit()
+        elif user_input1 == '2':
+            customer_dict[account_no].withdrawl()
+        elif user_input1 == '3':
+            mobile = int(input('Enter the mobile number of recepient: '))
+            if mobile in mobile_acc_link.keys():
+                secondary = mobile_acc_link[mobile]             # use mobile no. to get acct. no.
+                customer_dict[account_no].payment(customer_dict[secondary])
+            else:
+                print('The mobile number you have enter does not have an account associated with it')
+        elif user_input1 == '4':
+            i = datetime.datetime.now()
+            print('Logged Out On',i)
+            return
+        else:
+            print('Invalid input try again')
+        print('\n---------------------------------------------------------------\n')
+        customer_dict[account_no].basic_details()
+
+while True:
+    user_input1 = input('''Press 1 for creating a new customer:
+Press 2 for logging in as an existing customer:
+Press 3 for displaying number of customers:
+Press 4 for exit\n''')
+
+    if user_input1 == '1':
+        print('Create user')
+        new_cust()
+    elif user_input1 == '2':
+        login()
+    elif user_input1 == '3':
+        print('There currently', BankAccount.no_of_cust,'customers in State Bank of India.')
+    elif user_input1 == '4':
+        print('Exited')
+        break
+    else:
+        print('Invalid input try again')
+    print('\n-------------------------------------------------------------\n')
